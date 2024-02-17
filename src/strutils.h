c8cacbe33eca6bb10c8e655899bac84dc07ff551
Grzegorz Kraszewski <grzegorz.kraszewski@gmail.com>
2024-02-17 22:27:43 +0100

    Changed type of varargs to LONG. Added variadic FmtPut().

--- src/strutils.h
+++ src/strutils.h
@@ -3,10 +3,11 @@
 #define StrAlloc(len) (STRPTR)AllocVec((len), MEMF_ANY)
 #define StrFree(str) FreeVec(str)
 
-ULONG VFmtLen(STRPTR fmt, APTR *args);
-void VFmtPut(STRPTR dest, STRPTR fmt, APTR *args);
-STRPTR VFmtNew(STRPTR fmt, APTR *args);
-STRPTR FmtNew(STRPTR fmt, APTR arg1, ...);
+ULONG VFmtLen(STRPTR fmt, LONG *args);
+void VFmtPut(STRPTR dest, STRPTR fmt, LONG *args);
+void FmtPut(STRPTR dest, STRPTR fmt, LONG arg1, ...);
+STRPTR VFmtNew(STRPTR fmt, LONG *args);
+STRPTR FmtNew(STRPTR fmt, LONG arg1, ...);
 ULONG StrLen(STRPTR s);
 STRPTR StrCopy(STRPTR s, STRPTR d);
 STRPTR StrClone(STRPTR s);
